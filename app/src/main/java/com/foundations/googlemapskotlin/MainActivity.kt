b09234b573d382c9cbd559eea84cbf788ff7ac55
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-10-05 13:46:55 -0700

    Initial commit

---
+++ app/src/main/java/com/foundations/googlemapskotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.foundations.googlemapskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
